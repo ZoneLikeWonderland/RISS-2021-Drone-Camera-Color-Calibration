cd7ba4dc95e3c3f56b0b323b26f1ce88662726e0
zlw <zhanglw2@shanghaitech.edu.cn>
2021-07-23 18:59:39 -0400

    first commit from nuc

---
+++ ros_deploy_workspace/src/camera_color_fe/scripts/postprocess.py
@@ -0,0 +1,338 @@
+# import cv2.ximgproc
+from functools import cmp_to_key
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+# x_base, y_base = np.meshgrid(range(320), range(240))
+x_base, y_base = None, None
+
+
+AREA_THRESHOLD = 0.1
+STD_THRESHOLD = 0.1
+LINE_MIN_LENGTH = 4
+
+plt.ion()
+
+
+def pickout(raw, rect, c,show_list):
+
+    global x_base, y_base
+    if x_base is None:
+        x_base, y_base = np.meshgrid(range(c.shape[1]), range(c.shape[0]))
+
+    if rect is not None:
+        rect = cv2.dilate(rect, np.ones((10, 10)))
+        rect_b = (rect > 0.05).astype(np.uint8)
+        #cv2.imshow("rect_b", rect_b * 255)
+        retval, labels, stats, centroids = cv2.connectedComponentsWithStats(
+            rect_b)
+        bgid = labels[rect_b == 0].min()
+        stats[bgid, 4] = -1
+        tgtid = np.argmax(stats[:, -1])
+        rect[labels != tgtid] = 0
+        c *= rect
+
+    c_mask = (c > 0.1).astype(np.uint8) * 255
+
+    #cv2.imshow("c_mask", c_mask)
+
+    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(c_mask)
+
+    ps = []
+
+    c_show = c.copy()
+
+    for i in range(1, retval):
+        mask = (labels == i).astype(np.uint8)
+        mask = cv2.dilate(mask, np.ones((10, 10)))
+        pc = mask * c
+        x = (pc * x_base).sum() / pc.sum()
+        y = (pc * y_base).sum() / pc.sum()
+        ps.append((-pc.sum(), (x, y)))
+        cv2.circle(c_show, (int(x), int(y)), 10, 0.2)
+
+    ps = [i[1] for i in sorted(ps)[:4]]
+    ps = np.array(ps)
+    center = ps.mean(axis=0)
+    # print("center", center)
+    ps = sorted(ps, key=lambda p: np.arctan2((p - center)[1], (p - center)[0]))
+    ps = np.array(ps)
+
+    for i, p in enumerate(ps):
+        cv2.circle(c_show, (int(p[0]), int(p[1])), 10, 255)
+        cv2.putText(c_show, str(i), (int(p[0]), int(
+            p[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+    #cv2.imshow("c_show", c_show)
+    show_list["c_show"]=c_show
+
+    if len(ps) < 4:
+        return
+
+    ps[:, 0] = ps[:, 0] / c.shape[1] * raw.shape[1]
+    ps[:, 1] = ps[:, 1] / c.shape[0] * raw.shape[0]
+
+    tw, th = 200, 200
+    cp = np.array([(0, 0), (tw, 0), (tw, th), (0, th)])
+
+    H, _ = cv2.findHomography(
+        ps,
+        cp
+    )
+
+    warp = cv2.warpPerspective(raw, H, (tw, th))
+    #cv2.imshow("warp", warp)
+    show_list["warp"]=warp
+
+    warp = warp.astype(np.float32)
+
+    canny = cv2.Canny((warp * 255).astype(np.uint8), 30, 200)
+    canny = cv2.dilate(canny, None)
+    canny = cv2.erode(canny, None)
+    canny = 255 - canny
+
+    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(
+        canny, connectivity=4)
+    area = stats[:, -1:]
+    widths = stats[:, 2]
+    heights = stats[:, 3]
+
+    expected_area = widths * heights
+    exp_ratio = expected_area / area[:, 0]
+    # print(exp_ratio)
+    reject = (exp_ratio > 1 + AREA_THRESHOLD) | (exp_ratio < 1 - AREA_THRESHOLD)
+    # print(reject)
+    stats_id = np.concatenate((
+        area,
+        np.arange(0, retval)[:, None],
+    ), axis=1)
+
+    stats_ratio = np.array(sorted(
+        stats_id.tolist()
+    ), dtype=np.float32)
+    if len(stats) <= 2:
+        return
+    stats_ratio[1:, 0] /= stats_ratio[:-1, 0]
+    stats_ratio[0, 0] = 0
+
+    # print(stats_ratio)
+
+    start = None
+    end = None
+
+    best = (0, None, None)
+
+    notbad = []
+
+    for i in range(1, retval):
+        if start is None and stats_ratio[i, 0] < 1.1:
+            start = i - 1
+        if end is None and start is not None and stats_ratio[i, 0] > 1.1:
+            end = i
+            best = max(best, (end - start, start, end))
+            if end - start > 4:
+                notbad.append((end - start, start, end))
+            start = None
+            end = None
+
+    new_label = np.zeros_like(labels, np.float32)
+    select = []
+
+    length, start, end = best
+    if start is None or end is None:
+        return
+
+    for length, start, end in notbad:
+        for i in range(start, end):
+            c = int(stats_ratio[i, 1])
+            if reject[c]:
+                continue
+            std = np.std(warp[labels == c], axis=0)
+            if (std > STD_THRESHOLD).any():
+                continue
+            if (labels == c).sum() < tw*th/40:
+                continue
+
+            select.append(c)
+
+            new_label[labels == c] = c * 0.02
+            cv2.putText(new_label, str(c), tuple(
+                [int(i) for i in centroids[c]]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    #cv2.imshow("canny_labels", new_label)
+    show_list["canny_labels"]=new_label
+
+    #cv2.imshow("canny", canny)
+    show_list["canny"]=canny
+    if len(select) <= 2:
+        return
+
+    def get_half(ar):
+        xs = np.array(sorted(ar))
+        # print(xs)
+        xs = np.array(sorted(xs[1:] - xs[:-1]))
+        half = xs[xs > xs.mean()].mean() / 2
+        # print(xs, half)
+        jl = np.zeros(ar.shape, np.int)
+        # jl2 = np.zeros(ar.shape, np.int)
+        for i in range(ar.shape[0]):
+            jl[(ar < ar[i] + half) & (ar > ar[i] - half)] = i
+        # print(ar)
+
+        ms = []
+        for i, j in enumerate(sorted(np.unique(jl))):
+            ms.append(ar[jl == j].mean())
+        ms = sorted(ms)
+
+        return ms, half
+
+    xs, x_half = get_half(centroids[select][:, 0])
+    ys, y_half = get_half(centroids[select][:, 1])
+
+    for x in xs:
+        cv2.line(new_label, (int(x), 0), (int(x), 999), 0.6)
+    for y in ys:
+        cv2.line(new_label, (0, int(y)), (999, int(y)), 0.6)
+
+    width = np.median(widths[select])
+    height = np.median(heights[select])
+    # print(widths, heights)
+    scale = 0.4
+
+    matrix = np.zeros((len(ys), len(xs), 3))
+    matrix_use = np.zeros((len(ys), len(xs)))
+
+    for i, x in enumerate(xs):
+        for j, y in enumerate(ys):
+            select_area = np.zeros_like(new_label)
+            cv2.circle(select_area, (int(x), int(y)), int(
+                min(width, height) * scale), 255, -1)
+            data = warp[select_area > 0]
+            std = np.std(data, axis=0)
+            mean = np.mean(data, axis=0)
+            # print(std)
+            if (std > STD_THRESHOLD).any():
+                continue
+
+            matrix[j, i] = mean
+            matrix_use[j, i] = 1
+
+            cv2.circle(new_label, (int(x), int(y)), int(
+                min(width, height) * scale), 0.6, 1)
+
+    #cv2.imshow("canny_labels", new_label)
+    show_list["canny_labels"]=new_label
+    matrix_show=cv2.resize(
+        matrix, warp.shape[:2], interpolation=cv2.INTER_NEAREST)
+    #cv2.imshow("matrix", matrix_show)
+    show_list["matrix"]=matrix_show
+
+    best = (-1, None, None, None)
+
+    for i, x in enumerate(xs):
+        line = matrix[:, i][matrix_use[:, i] > 0]
+        if line.shape[0] < LINE_MIN_LENGTH:
+            continue
+        diff = line[1:] - line[:-1]
+        diff *= 1 if diff.mean() > 0 else - 1
+        if diff.min() < 0:
+            continue
+        crstd = np.std(line / line.mean(axis=1)[..., None], axis=0).sum()
+        # plt.plot(line/line.mean(axis=1)[...,None])
+        # plt.show()
+        best = max(best, (-crstd, (slice(None, None, None), i), line, diff))
+
+    for j, y in enumerate(ys):
+        line = matrix[j][matrix_use[j] > 0]
+        if line.shape[0] < LINE_MIN_LENGTH:
+            continue
+        diff = line[1:] - line[:-1]
+        diff *= 1 if diff.mean() > 0 else - 1
+        if diff.min() < 0:
+            continue
+        crstd = np.std(line / line.mean(axis=1)[..., None], axis=0).sum()
+        # plt.plot(line/line.mean(axis=1)[...,None])
+        # plt.show()
+        best = max(best, (-crstd, (j, slice(None, None, None)), line, diff))
+
+    show_key = np.zeros((len(ys), len(xs)))
+
+    # print(best)
+
+    _, idx, line, diff = best
+    if idx is None:
+        return
+    show_key[idx] = 1
+
+    good = np.ones(line.shape[0], np.int)
+    good[0] = 0
+    good[-1] = 0
+
+    for i in range(len(diff)):
+        if (diff[i] <= 0).any():
+            good[i] = 0
+            good[i + 1] = 0
+
+    show_key[idx][matrix_use[idx] > 0] = good * 2
+
+    for i, x in enumerate(xs):
+        for j, y in enumerate(ys):
+            if matrix_use[j, i] > 0:
+                if show_key[j, i] == 1:
+                    cv2.circle(new_label, (int(x), int(y)), int(
+                        min(width, height) * scale), 0.8, 2)
+                if show_key[j, i] == 2:
+                    cv2.circle(new_label, (int(x), int(y)), int(
+                        min(width, height) * scale), 1, 4)
+
+    # print(show_key)
+
+    #cv2.imshow("canny_labels", new_label)
+    show_list["canny_labels"]=new_label
+
+    # print(good)
+    to_be_calibrate = line[good > 0]
+    # print(to_be_calibrate)
+    coeff = 1 / to_be_calibrate
+    coeff /= coeff.min(axis=1)[..., None]
+    coeff = coeff.mean(axis=0)
+    # print(coeff)
+
+    warp_calibrated = coeff * warp
+    #cv2.imshow("warp_calibrated", warp_calibrated)
+    show_list["warp_calibrated"]=warp_calibrated
+
+    cv2.waitKey(1)
+
+    bright = warp.mean()
+
+    x = ps[:, 0].mean()
+    y = ps[:, 1].mean()
+
+    dist = ((x-raw.shape[1]/2)**2+(y-raw.shape[0]/2)**2)**0.5\
+        / max(raw.shape)
+
+    plt.scatter(dist, bright)
+    plt.draw()
+    plt.pause(0.01)
+
+    return coeff, [dist.item(), bright.item()]
+
+
+if __name__ == "__main__":
+
+    raw = cv2.imread(r"C:\Users\pc-16\Downloads\image_left_03.png") / 255
+    r = cv2.imread(r"C:\Users\pc-16\Downloads\pred_r.png",
+                   cv2.IMREAD_GRAYSCALE) / 255
+    c = cv2.imread(r"C:\Users\pc-16\Downloads\pred_c.png",
+                   cv2.IMREAD_GRAYSCALE) / 255
+
+    # # pickout(raw, r, c)
+    # raw = cv2.imread(r"C:\Users\pc-16\Downloads\image_left_03_blur.png") / 255
+
+    # raw = cv2.imread(r"E:\RISS\img.png") / 255
+    # r = cv2.imread(r"E:\RISS\pred.png", cv2.IMREAD_GRAYSCALE) / 255
+    # c = cv2.imread(r"E:\RISS\pred_c.png", cv2.IMREAD_GRAYSCALE) / 255
+    coeff = pickout(raw, r, c)
+    cv2.imwrite("output.png", raw * coeff * 255)
+    cv2.waitKey()
+
---
+++ ros_deploy_workspace/src/camera_color_fe/src/retailer.cpp
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// %Tag(FULLTEXT)%
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sensor_msgs/Image.h>
+#include <camera_color_fe/colorir.h>
+#include <opencv2/opencv.hpp>
+
+#include <inference_engine.hpp>
+#include <chrono>
+
+using namespace InferenceEngine;
+
+ros::Publisher retailer_pub;
+
+std::string input_name;
+std::string output_name;
+ExecutableNetwork executable_network;
+
+
+
+InferenceEngine::Blob::Ptr wrapMat2Blob_f32(const cv::Mat &mat) {
+    size_t channels = mat.channels();
+    size_t height = mat.size().height;
+    size_t width = mat.size().width;
+
+    InferenceEngine::TensorDesc tDesc(InferenceEngine::Precision::FP32,
+                                      {1, channels, height, width},
+                                      InferenceEngine::Layout::NHWC);
+
+    return InferenceEngine::make_shared_blob<float>(tDesc, (float*)mat.data);
+}
+
+
+
+sensor_msgs::Image Mat2Image(cv::Mat frame_now,int channel=3){
+
+    sensor_msgs::Image output_image_msg;
+
+
+                    output_image_msg.height=frame_now.rows;
+                    output_image_msg.width=frame_now.cols;
+                    output_image_msg.encoding="bgr8";
+                    output_image_msg.is_bigendian=false;
+                    output_image_msg.step=frame_now.cols*channel;
+                    size_t size = output_image_msg.step * frame_now.rows;
+                    output_image_msg.data.resize(size);
+                    memcpy((char*)(&output_image_msg.data[0]), frame_now.data, size);
+
+return output_image_msg;
+}
+
+void imageCallback(const sensor_msgs::Image::ConstPtr& image_msg){
+    //cv::Mat color_mat(image_msg->height,image_msg->width,CV_MAKETYPE(CV_8U,3),const_cast<uchar*>(&image_msg->data[0]), image_msg->step);
+    //cv::cvtColor(color_mat,color_mat,cv::COLOR_BGR2RGB);
+    ROS_INFO("I heard a image %dx%d", image_msg->width, image_msg->height);
+    cv::Mat bayer_mat(image_msg->height,image_msg->width,CV_8U,const_cast<uchar*>(&image_msg->data[0]), image_msg->step);
+    cv::Mat color_mat;
+
+    cv::cvtColor(bayer_mat,color_mat,cv::COLOR_BayerRG2BGR);
+cv::resize(color_mat,color_mat,{color_mat.cols/2,color_mat.rows/2});
+
+
+
+InferRequest infer_request = executable_network.CreateInferRequest();
+        cv::Mat image_f32;
+        color_mat.convertTo(image_f32,CV_32FC3,1.0/255);
+        Blob::Ptr imgBlob = wrapMat2Blob_f32(image_f32);  // just wrap Mat data by Blob::Ptr without allocating of new memory
+        infer_request.SetBlob(input_name, imgBlob);  // infer_request accepts input blob of any size
+
+            auto start=std::chrono::steady_clock::now();
+//            std::cout<<clock()*1.0/CLOCKS_PER_SEC<<"\n";
+            infer_request.Infer();
+//            std::cout<<clock()*1.0/CLOCKS_PER_SEC<<"\n";
+            auto duration=std::chrono::steady_clock::now()-start;
+            std::cout<<"time = "<<std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()<<"ms\n";
+
+
+        Blob::Ptr output = infer_request.GetBlob(output_name);
+        auto raw_ptr=output->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+        auto outbox=cv::Mat(480,640,CV_32F,raw_ptr);
+       // cv::imwrite("outbox.png",outbox*255);
+        auto outptr=cv::Mat(480,640,CV_32F,raw_ptr+640*480);
+      //  cv::imwrite("outptr.png",outptr*255);
+
+cv::Mat outbox_8u;
+outbox.convertTo(outbox_8u,CV_8UC1,255);
+
+cv::Mat outptr_8u;
+outptr.convertTo(outptr_8u,CV_8UC1,255);
+
+camera_color_fe::colorir ir;
+
+
+
+ir.raw=Mat2Image(color_mat);
+ir.block=Mat2Image(outbox_8u,1);
+ir.point=Mat2Image(outptr_8u,1);
+
+                    retailer_pub.publish(ir);
+
+
+
+}
+// %EndTag(CALLBACK)%
+
+int main(int argc, char **argv)
+{
+
+  Core ie;
+  std::string input_model="/zlwtest/deploy/card/card.xml";
+  CNNNetwork network = ie.ReadNetwork(input_model);
+
+        InputInfo::Ptr input_info = network.getInputsInfo().begin()->second;
+         input_name = network.getInputsInfo().begin()->first;
+
+        input_info->getPreProcess().setResizeAlgorithm(RESIZE_BILINEAR);
+        input_info->setLayout(Layout::NHWC);
+        input_info->setPrecision(Precision::FP32);
+
+       DataPtr output_info = network.getOutputsInfo().begin()->second;
+        output_name = network.getOutputsInfo().begin()->first;
+
+        output_info->setPrecision(Precision::FP32);
+
+std::string device_name="CPU";
+     executable_network = ie.LoadNetwork(network, device_name);
+
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "retailer");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /**
+   * The subscribe() call is how you tell ROS that you want to receive messages
+   * on a given topic.  This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing.  Messages are passed to a callback function, here
+   * called chatterCallback.  subscribe() returns a Subscriber object that you
+   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
+   * object go out of scope, this callback will automatically be unsubscribed from
+   * this topic.
+   *
+   * The second parameter to the subscribe() function is the size of the message
+   * queue.  If messages are arriving faster than they are being processed, this
+   * is the number of messages that will be buffered up before beginning to throw
+   * away the oldest ones.
+   */
+// %Tag(SUBSCRIBER)%
+  //retailer_pub = n.advertise<sensor_msgs::Image>("retailer", 1);
+  retailer_pub = n.advertise<camera_color_fe::colorir>("retailer", 1);
+  ros::Subscriber sub = n.subscribe("/xic_stereo/left/image_raw", 1, imageCallback);
+// %EndTag(SUBSCRIBER)%
+
+  /**
+   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+   * callbacks will be called from within this thread (the main one).  ros::spin()
+   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+   */
+// %Tag(SPIN)%
+  ros::spin();
+// %EndTag(SPIN)%
+
+  return 0;
+}
+// %EndTag(FULLTEXT)%
+
